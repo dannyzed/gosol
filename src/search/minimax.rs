6f9d2657059c825013e01d05affc9411320c4d22
Daniel Zawada <zawadadaniel@gmail.com>
2016-03-19 19:23:28 -0600

    Added another tsumego test
    Currently it doesnt pass, we need to add capture scoring to the board

--- src/search/minimax.rs
+++ src/search/minimax.rs
@@ -76,4 +76,31 @@ mod tests {
         assert_eq!(mv.unwrap(), GoMove::Move{x: 1, y: 0, player: State::Black});
 
     }
+
+    #[test]
+    fn corner_kill() {
+        let mut game_board = SimpleBoard::new(7, 5);
+
+        let black_x = vec![0, 1, 2, 3, 4, 5, 5, 5];
+        let black_y = vec![3, 3, 3, 2, 2, 2, 1, 0];
+
+        for (x, y) in black_x.iter().zip(black_y.iter()) {
+            let mv = GoMove::Move{x: *x, y: *y, player: State::Black};
+            game_board.play_move(&mv);
+        }
+
+        let white_x = vec![0, 1, 2, 3, 4, 1];
+        let white_y = vec![2, 2, 2, 1, 1, 0];
+
+        for (x, y) in white_x.iter().zip(white_y.iter()) {
+            let mv = GoMove::Move{x: *x, y: *y, player: State::White};
+            game_board.play_move(&mv);
+        }
+
+        let (_, mv) = minimax(&mut game_board, 7, true);
+
+        assert_eq!(mv.unwrap(), GoMove::Move{x: 0, y: 0, player: State::Black});
+
+    }
+
 }
